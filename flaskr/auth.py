b1c3a33dde0cf2ae64f624d0fddc4842474f8ed4
Aviral Jain <aviraljain99@gmail.com>
2022-05-23 16:29:34 +1000

    Finished coding the blog - need to follow next steps of the tutorial

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -39,7 +39,7 @@ def register():
     return render_template('auth/register.html')
 
 
-@bp.route('/login', mmethods=('GET', 'POST'))
+@bp.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
         username = request.form['username']
